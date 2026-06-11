abc06307c81798c4e336c03d9ce592aff3b96aad
Geng <3348117240@qq.com>
2026-06-11 23:03:27 +0800

    feat:一点毛病没有

--- BSP/jdy28m_geng/mesh_mode.c
+++ BSP/jdy28m_geng/mesh_mode.c
@@ -325,24 +325,25 @@ jdy_status_t JDY_func_init(Jdy_t *const self)
 #endif
     self->p_time->my_delay(200);
 
-    if(self->p_mesh_submode->MADDR[4]=='\r') {
-#ifdef JDY_DEBUG
-    JDY_DEBUG_OUT("JDY_OK\n");
-#endif
-        self->p_mesh_submode->mash_init_flag = JDY_INIT;
-    }else{
+//     if(self->p_mesh_submode->MADDR[4]=='\r') {
+// #ifdef JDY_DEBUG
+//     JDY_DEBUG_OUT("JDY_OK\n");
+// #endif
+//         self->p_mesh_submode->mash_init_flag = JDY_INIT;
+//     }else{
 
-#ifdef JDY_DEBUG
-    JDY_DEBUG_OUT("JDY_ERROR:MADDR[4]=%c\n", self->p_mesh_submode->MADDR[4]);
-#endif
-        self->p_mesh_submode->mash_init_flag = JDY_NOT_INIT;
-        res = JDY_ERROR;
-        my_4g_dtu.jdy_error_flag = 1;
-    }
+// // #ifdef JDY_DEBUG
+// //     JDY_DEBUG_OUT("JDY_ERROR:MADDR[4]=%c\n", self->p_mesh_submode->MADDR[4]);
+// // #endif
+//         self->p_mesh_submode->mash_init_flag = JDY_NOT_INIT;
+//         res = JDY_ERROR;
+//         my_4g_dtu.jdy_error_flag = 1;
+//     }
     /*************5. Resetting JDY-28M and marking initialization complete**************/
     dev_OK(self, "AT+RESET\r\n");
     self->p_time->my_delay(1000);
      // 标记MESH初始化完成
+		 self->p_mesh_submode->mash_init_flag = JDY_INIT;
     /*************5. Resetting JDY-28M and marking initialization complete**************/
 
     return res;
@@ -745,6 +746,9 @@ jdy_status_t jdy_task(Jdy_t *const self, mesh_datasend_pkt_t *pkt, ProtocolData 
         pkt->R = 2;
         pkt->valid = 0x01; // user  //key
         res = jdy_handle.p_mesh_submode->p_parser->pf_mesh_datasend_handler(self, pkt);
+#ifdef JDY_DEBUG
+        JDY_DEBUG_OUT("Middle Car 1 received forward signal, sent response to Car 1.\n");
+#endif
         
 
     }else if (2 == self->p_mesh_submode->p_parser->recv_pkt.L)
@@ -755,6 +759,9 @@ jdy_status_t jdy_task(Jdy_t *const self, mesh_datasend_pkt_t *pkt, ProtocolData 
         pkt->R = 3;
         pkt->valid = 0x01; // user  //key
         res = jdy_handle.p_mesh_submode->p_parser->pf_mesh_datasend_handler(self, pkt);
+#ifdef JDY_DEBUG
+        JDY_DEBUG_OUT("Middle Car 1 received return signal, sent response to Car 4.\n");
+#endif
     }
 
 #endif
